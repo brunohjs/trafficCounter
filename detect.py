0dbd6df6f31aeda654d00b8647912667745dee87
Bruno Justo <brunohjs@gmail.com>
2018-05-30 18:13:42 -0300

    Criação função detectVehicle

--- detect.py
+++ detect.py
@@ -2,8 +2,25 @@ import numpy as np
 import cv2
 import sys
 
+def detectVehicle(stat, centroid):
+    area = stat[cv2.CC_STAT_AREA]
+    initial_point = (stat[cv2.CC_STAT_LEFT], stat[cv2.CC_STAT_TOP])
+    initial_point_text = (initial_point[0], initial_point[1] - 5)
+    final_point = (initial_point[0] + stat[cv2.CC_STAT_WIDTH], initial_point[1] + stat[cv2.CC_STAT_HEIGHT])
+    #candidates.append((initial_point, final_point, area))
+    
+    if inSquare(road_points, centroid, 'bottom-up'):
+        cv2.rectangle(frame, initial_point, final_point, (0, 0, 255), 1)
+        cv2.circle(frame, centroid, 3, (0,255,255), -1)
+        cv2.putText(
+            frame, 
+            classify(area, stat[cv2.CC_STAT_WIDTH], stat[cv2.CC_STAT_HEIGHT]),
+            initial_point_text, 
+            cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 255), 1
+        )
 
-def detectVehicle(area, width, height):
+
+def classify(area, width, height):
     car = (area <= min_area+(min_area*1.0)) and (area > min_area)
     truck = area > min_area*4
     if car:
@@ -38,7 +55,7 @@ road_points = [[90,180], [5,244], [202,244], [214,180]]
 road_area = np.array(road_points, np.int32)
 road_area = road_area.reshape((-1,1,2))
 
-cv2.namedWindow("BK");
+cv2.namedWindow("BK")
 cv2.moveWindow("BK", 400, 0)
 
 while True:
@@ -56,28 +73,14 @@ while True:
     for i in range(len(stats)):
         stat = stats[i]
         centroid = (int(centroids[i][0]), int(centroids[i][1]))
-        area = stat[cv2.CC_STAT_AREA]
-        
+
         '''
         if classify(area, stat[cv2.CC_STAT_WIDTH], stat[cv2.CC_STAT_HEIGHT]) != 'truck':
             print((area, stat[cv2.CC_STAT_WIDTH], stat[cv2.CC_STAT_HEIGHT]), classify(area, stat[cv2.CC_STAT_WIDTH], stat[cv2.CC_STAT_HEIGHT]))
         '''
 
-        if area >= min_area:
-            initial_point = (stat[cv2.CC_STAT_LEFT], stat[cv2.CC_STAT_TOP])
-            initial_point_text = (initial_point[0], initial_point[1] - 5)
-            final_point = (initial_point[0] + stat[cv2.CC_STAT_WIDTH], initial_point[1] + stat[cv2.CC_STAT_HEIGHT])                                                
-            candidates.append((initial_point, final_point, area))
-            
-            if inSquare(road_points, centroid, 'bottom-up'):
-                cv2.rectangle(frame, initial_point, final_point, (0, 0, 255), 1)
-                cv2.circle(frame, centroid, 3, (0,255,255), -1)
-                cv2.putText(
-                    frame, 
-                    detectVehicle(area, stat[cv2.CC_STAT_WIDTH], stat[cv2.CC_STAT_HEIGHT]),
-                    initial_point_text, 
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 255), 1
-                )
+        if stat[cv2.CC_STAT_AREA] >= min_area:
+            detectVehicle(stat, centroid)
 
     #cv2.putText(frame,'COUNT: %r' %i, (10,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
     cv2.imshow("Track", frame)
