2ee649d42ca5f2de09903e98bf8d4b2bd1117533
Bruno Justo <brunohjs@gmail.com>
2018-05-30 19:39:16 -0300

    Inclusão da função de evento do click do mouse (desativada por enquanto)

--- detect.py
+++ detect.py
@@ -2,22 +2,36 @@ import numpy as np
 import cv2
 import sys
 
-def detectVehicle(stat, centroid):
-    area = stat[cv2.CC_STAT_AREA]
-    initial_point = (stat[cv2.CC_STAT_LEFT], stat[cv2.CC_STAT_TOP])
-    initial_point_text = (initial_point[0], initial_point[1] - 5)
-    final_point = (initial_point[0] + stat[cv2.CC_STAT_WIDTH], initial_point[1] + stat[cv2.CC_STAT_HEIGHT])
-    #candidates.append((initial_point, final_point, area))
+'''
+def on_mouse(event, x, y, buttons, user_param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        polygon.append([x, y])
+'''
+
+def detectVehicle(stats, centroids, frame):
+    for i in range(len(stats)):
+        stat = stats[i]
+        area = stat[cv2.CC_STAT_AREA]
+        centroid = (int(centroids[i][0]), int(centroids[i][1]))
+
+        if area >= min_area:
+            
+            initial_point = (stat[cv2.CC_STAT_LEFT], stat[cv2.CC_STAT_TOP])
+            initial_point_text = (initial_point[0], initial_point[1] - 5)
+            final_point = (initial_point[0] + stat[cv2.CC_STAT_WIDTH], initial_point[1] + stat[cv2.CC_STAT_HEIGHT])
+            #candidates.append((initial_point, final_point, area))
+            
+            if inSquare(road_points, centroid, 'bottom-up'):
+                cv2.rectangle(frame, initial_point, final_point, (0, 0, 255), 1)
+                cv2.circle(frame, centroid, 3, (0,255,255), -1)
+                cv2.putText(
+                    frame, 
+                    classify(area, stat[cv2.CC_STAT_WIDTH], stat[cv2.CC_STAT_HEIGHT]),
+                    initial_point_text, 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 255), 1
+                )
     
-    if inSquare(road_points, centroid, 'bottom-up'):
-        cv2.rectangle(frame, initial_point, final_point, (0, 0, 255), 1)
-        cv2.circle(frame, centroid, 3, (0,255,255), -1)
-        cv2.putText(
-            frame, 
-            classify(area, stat[cv2.CC_STAT_WIDTH], stat[cv2.CC_STAT_HEIGHT]),
-            initial_point_text, 
-            cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 255), 1
-        )
+    return frame
 
 
 def classify(area, width, height):
@@ -45,42 +59,38 @@ def inSquare(area, point, way):
             return False
 
 
-
 VIDEO_SOURCE = sys.argv[1]
+min_area = 500
 
-capture = cv2.VideoCapture(VIDEO_SOURCE) 
+capture = cv2.VideoCapture(VIDEO_SOURCE)
 backsub = cv2.bgsegm.createBackgroundSubtractorMOG()
 
-road_points = [[90,180], [5,244], [202,244], [214,180]]
+#cv2.setMouseCallback("Draw Polygon", on_mouse)
+
+road_points = [[90,180], [5,244], [202,244], [214,180]]     #video.mp4
 road_area = np.array(road_points, np.int32)
 road_area = road_area.reshape((-1,1,2))
 
-cv2.namedWindow("BK")
-cv2.moveWindow("BK", 400, 0)
+cv2.namedWindow('Background')
+cv2.moveWindow('Background', 400, 0)
+cv2.namedWindow('Track')
 
 while True:
     ret, frame = capture.read()
-
+    
     bkframe = backsub.apply(frame, None, 0.01)
-    bkframe = cv2.medianBlur(bkframe, 9)
+    bkframe = cv2.medianBlur(bkframe, 7)
     bkframe = cv2.blur(bkframe, (7,7))
     cv2.polylines(frame, [road_area], True, (0,255,0), 3)
 
     num, labels, stats, centroids = cv2.connectedComponentsWithStats(bkframe, ltype=cv2.CV_16U)
-    min_area = 500
     candidates = list()
 
-    for i in range(len(stats)):
-        stat = stats[i]
-        centroid = (int(centroids[i][0]), int(centroids[i][1]))
-
-        if stat[cv2.CC_STAT_AREA] >= min_area:
-            detectVehicle(stat, centroid)
+    frame = detectVehicle(stats, centroids, frame)
 
     #cv2.putText(frame,'COUNT: %r' %i, (10,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-    cv2.imshow("Track", frame)
-    cv2.imshow('BK', bkframe)
+    cv2.imshow('Track', frame)
+    cv2.imshow('Background', bkframe)
 
-    key = cv2.waitKey(100)
-    if key == ord('q'):
+    if cv2.waitKey(100) == ord('q'):
             break
