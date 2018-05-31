32673c4a43ca9d92525f2efdce2b3cf75a98dd10
Bruno Justo <brunohjs@gmail.com>
2018-05-31 01:03:32 -0300

    Módulo de funções algébricas

---
+++ cartesian.py
@@ -0,0 +1,15 @@
+import math
+
+def distance(p1, p2):
+    return math.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
+
+def vector(p1, p2):
+    return (p1[0] - p2[0], p1[1] - p2[1])
+
+def module(vector):
+    return math.sqrt(vector[0]**2 + vector[1]**2)
+
+def angleVectors(u, v):
+    scalar = u[0] * v[0] + u[1] * v[1]
+    mod = module(u) * module(v)
+    return math.degrees(math.cos(scalar/mod))
