37d3c0ff390d46f8a0b46f60ea3771f5c3a6203a
Bruno Justo <brunohjs@gmail.com>
2018-06-03 20:37:49 -0300

    Criação do código 'detect.py'

---
+++ detect.py
@@ -0,0 +1,59 @@
+import Vehicle
+import main
+import cv2
+from cartesian import distance
+
+def detectVehicle(stats, centroids, frame, frame_id, buffer_frames, vehicles, road_points):
+    points = list()
+    for i in range(len(stats)):
+        stat = stats[i]
+        area = stat[cv2.CC_STAT_AREA]
+        centroid = (int(centroids[i][0]), int(centroids[i][1]))
+
+        if (area >= main.MIN_AREA) and inSquare(road_points, centroid, 'bottom-up'):
+            if not vehicles:
+                vehicles.append(Vehicle.Vehicle(len(vehicles), frame_id, centroid, stat))
+            else:
+                points.append([centroid, stat])
+            
+    if buffer_frames:
+        vehicles = findVehicles(vehicles, buffer_frames[0], frame_id, 30)
+    print(points)
+    if points:
+        buffer_frames = addFrame(points, buffer_frames, 1)
+    return buffer_frames, frame
+
+def inSquare(road_area, point, way):
+    high_y = road_area[1][1]
+    low_y = road_area[0][1]
+    high_x = int((road_area[2][0]+road_area[3][0])/2) + 5
+    low_x = int((road_area[0][0]+road_area[1][0])/2) - 5
+    if way == 'bottom-up':
+        in_y = point[1] < high_y and point[1] > low_y
+        in_x = point[0] < high_x and point[0] > low_x
+        if in_x and in_y:
+            return True
+        else:
+            return False
+
+def findVehicles(vehicles, points, frame_id, max_distance=40):
+    for vehicle in vehicles:
+        found = False
+        for point in points:
+            if distance(vehicle.getCurrentPose(), point[0]) <= max_distance:
+                vehicle.setCurrentPose(point[0], point[1], frame_id)
+                points.remove(point)
+                found = True
+                break
+        if not found:
+            vehicle.incrementNoFrame()
+    if points:
+        for point in points:
+            vehicles.append(Vehicle.Vehicle(len(vehicles), frame_id, point[0], point[1]))
+    return vehicles
+
+def addFrame(frame, buffer, max_size=10):
+    if len(buffer) >= max_size:
+        buffer.pop()
+    buffer.insert(0, frame)
+    return buffer
--- main.py
+++ main.py
@@ -1,76 +1,22 @@
+
 import Vehicle
-from cartesian import distance
+import detect
+
 import numpy as np
 import cv2
 import sys
 import time
 
-VIDEO_SOURCE = sys.argv[1]
-MIN_AREA = 500
-
-'''
+"""
 def on_mouse(event, x, y, buttons, user_param):
     if event == cv2.EVENT_LBUTTONDOWN:
         polygon.append([x, y])
-'''   
-
-def detectVehicle(stats, centroids, frame, frame_id, buffer_frames, vehicles, road_points):
-    points = list()
-    for i in range(len(stats)):
-        stat = stats[i]
-        area = stat[cv2.CC_STAT_AREA]
-        centroid = (int(centroids[i][0]), int(centroids[i][1]))
-
-        if (area >= MIN_AREA) and inSquare(road_points, centroid, 'bottom-up'):
-            if not vehicles:
-                vehicles.append(Vehicle.Vehicle(len(vehicles), frame_id, centroid, stat))
-            else:
-                points.append([centroid, stat])
-            
-    if buffer_frames:
-        vehicles = findVehicles(vehicles, buffer_frames[0], frame_id, 30)
-    print(points)
-    if points:
-        buffer_frames = addFrame(points, buffer_frames, 1)
-    return buffer_frames, frame
-
-def inSquare(road_area, point, way):
-    high_y = road_area[1][1]
-    low_y = road_area[0][1]
-    high_x = int((road_area[2][0]+road_area[3][0])/2) + 5
-    low_x = int((road_area[0][0]+road_area[1][0])/2) - 5
-    if way == 'bottom-up':
-        in_y = point[1] < high_y and point[1] > low_y
-        in_x = point[0] < high_x and point[0] > low_x
-        if in_x and in_y:
-            return True
-        else:
-            return False
-
-def findVehicles(vehicles, points, frame_id, max_distance=40):
-    for vehicle in vehicles:
-        found = False
-        for point in points:
-            if distance(vehicle.getCurrentPose(), point[0]) <= max_distance:
-                vehicle.setCurrentPose(point[0], point[1], frame_id)
-                points.remove(point)
-                found = True
-                break
-        if not found:
-            vehicle.incrementNoFrame()
-    if points:
-        for point in points:
-            vehicles.append(Vehicle.Vehicle(len(vehicles), frame_id, point[0], point[1]))
-    return vehicles
-
-def addFrame(frame, buffer, max_size=10):
-    if len(buffer) >= max_size:
-        buffer.pop()
-    buffer.insert(0, frame)
-    return buffer
-
+"""   
 
 #def inCars(centroid):
+
+VIDEO_SOURCE = sys.argv[1]
+MIN_AREA = 500
 
 def main():
     vehicles = list()
@@ -99,7 +45,7 @@ def main():
 
         num, labels, stats, centroids = cv2.connectedComponentsWithStats(bkframe, ltype=cv2.CV_16U)
 
-        buffer_frames, frame = detectVehicle(stats, centroids, frame, frame_id, buffer_frames, vehicles, road_points)
+        buffer_frames, frame = detect.detectVehicle(stats, centroids, frame, frame_id, buffer_frames, vehicles, road_points)
         for vehicle in vehicles:
             if not vehicle.isHide():
                 vehicle.show()
@@ -111,7 +57,7 @@ def main():
 
         cv2.imshow('Track', frame)
         cv2.imshow('Background', bkframe)
-        time.sleep(0.5)
+        #time.sleep(0.5)
 
         if cv2.waitKey(100) == ord('q'):
                 break
